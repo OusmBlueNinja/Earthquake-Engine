be0a9f122b847ef592ce6d4d804c9a75c5ae31be
GigabiteStudios <ceo@gigabitestudios.xyz>
2025-12-23 20:43:45 -0600

    camera: Add inverse view and projection matrices

--- core/renderer/camera.h
+++ core/renderer/camera.h
@@ -6,6 +6,8 @@ typedef struct camera_t
 {
     mat4 view;
     mat4 proj;
+    mat4 inv_view;
+    mat4 inv_proj;
     vec3 position;
 } camera_t;
 
@@ -14,6 +16,8 @@ static inline camera_t camera_create(void)
     camera_t c;
     c.view = mat4_identity();
     c.proj = mat4_identity();
+    c.inv_view = mat4_identity();
+    c.inv_proj = mat4_identity();
     c.position = (vec3){0, 0, 0};
     return c;
 }
@@ -25,6 +29,7 @@ static inline void camera_set_perspective(camera_t *c,
                                           float far_plane)
 {
     c->proj = mat4_perspective(fov_radians, aspect, near_plane, far_plane);
+    c->inv_proj = mat4_inverse(c->proj);
 }
 
 static inline void camera_set_orthographic(camera_t *c,
@@ -36,6 +41,7 @@ static inline void camera_set_orthographic(camera_t *c,
                                            float far_plane)
 {
     c->proj = mat4_ortho(left, right, bottom, top, near_plane, far_plane);
+    c->inv_proj = mat4_inverse(c->proj);
 }
 
 static inline void camera_look_at(camera_t *c,
@@ -45,6 +51,7 @@ static inline void camera_look_at(camera_t *c,
 {
     c->position = position;
     c->view = mat4_lookat(position, target, up);
+    c->inv_view = mat4_inverse(c->view);
 }
 
 static inline void camera_move(camera_t *c, vec3 delta)
@@ -52,6 +59,12 @@ static inline void camera_move(camera_t *c, vec3 delta)
     c->position.x += delta.x;
     c->position.y += delta.y;
     c->position.z += delta.z;
+}
+
+static inline void camera_recalc_view(camera_t *c, vec3 target, vec3 up)
+{
+    c->view = mat4_lookat(c->position, target, up);
+    c->inv_view = mat4_inverse(c->view);
 }
 
 static inline const mat4 *camera_view(const camera_t *c)
@@ -62,6 +75,16 @@ static inline const mat4 *camera_view(const camera_t *c)
 static inline const mat4 *camera_proj(const camera_t *c)
 {
     return &c->proj;
+}
+
+static inline const mat4 *camera_inv_view(const camera_t *c)
+{
+    return &c->inv_view;
+}
+
+static inline const mat4 *camera_inv_proj(const camera_t *c)
+{
+    return &c->inv_proj;
 }
 
 static inline mat4 camera_view_proj(const camera_t *c)
