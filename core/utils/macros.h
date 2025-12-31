0c745cec1c3b21e88a7132797dcb4eb5590521d9
GigabiteStudios <ceo@gigabitestudios.xyz>
2025-12-31 00:38:36 -0600

    added asserts to gl state

--- core/utils/macros.h
+++ core/utils/macros.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 #define STR1(x) #x
 #define STR(x) STR1(x)
@@ -37,3 +42,52 @@
 #define DEFER(begin, end) for (int CAT(_i_, __LINE__) = ((begin), 0); !CAT(_i_, __LINE__); CAT(_i_, __LINE__) += 1, (end))
 
 #define STATIC_ASSERT(cond, name) typedef char CAT(static_assert_, name)[(cond) ? 1 : -1]
+
+#if defined(_MSC_VER)
+#include <intrin.h>
+#define DBG_BREAK() __debugbreak()
+#elif defined(__GNUC__) || defined(__clang__)
+#define DBG_BREAK() __builtin_trap()
+#else
+#define DBG_BREAK() abort()
+#endif
+
+#if !defined(ASSERT_ENABLED)
+#if defined(_DEBUG)
+#define ASSERT_ENABLED 1
+#else
+#define ASSERT_ENABLED 0
+#endif
+#endif
+
+
+static inline void assert_fail_impl(const char *expr, const char *file, int line, const char *func, const char *fmt, ...)
+{
+    fprintf(stderr, "\nASSERT FAILED\n  expr: %s\n  at:   %s:%d\n  fn:   %s\n", expr, file, line, func);
+    if (fmt && fmt[0])
+    {
+        fprintf(stderr, "  msg:  ");
+        va_list ap;
+        va_start(ap, fmt);
+        vfprintf(stderr, fmt, ap);
+        va_end(ap);
+        fprintf(stderr, "\n");
+    }
+    fflush(stderr);
+    DBG_BREAK();
+    abort();
+}
+
+#if ASSERT_ENABLED
+#define ASSERT(x) do { if (!(x)) assert_fail_impl(#x, __FILE__, (int)__LINE__, __func__, NULL); } while (0)
+#define ASSERTF(x, fmt, ...) do { if (!(x)) assert_fail_impl(#x, __FILE__, (int)__LINE__, __func__, (fmt), ##__VA_ARGS__); } while (0)
+#else
+#define ASSERT(x) do { (void)sizeof(x); } while (0)
+#define ASSERTF(x, fmt, ...) do { (void)sizeof(x); (void)sizeof(fmt); } while (0)
+#endif
+
+#define REQUIRE(x) do { if (!(x)) assert_fail_impl(#x, __FILE__, (int)__LINE__, __func__, NULL); } while (0)
+#define REQUIRE_MSG(x, msg_lit) do { if (!(x)) assert_fail_impl(#x, __FILE__, (int)__LINE__, __func__, "%s", (msg_lit)); } while (0)
+
+#define PANIC(msg_lit) do { assert_fail_impl("PANIC", __FILE__, (int)__LINE__, __func__, "%s", (msg_lit)); } while (0)
+#define PANICF(fmt, ...) do { assert_fail_impl("PANIC", __FILE__, (int)__LINE__, __func__, (fmt), ##__VA_ARGS__); } while (0)
