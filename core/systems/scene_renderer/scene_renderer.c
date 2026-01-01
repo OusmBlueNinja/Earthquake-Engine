2bdd2aca67eb215dbee78593a6fe47d700e91a5b
GigabiteStudios <ceo@gigabitestudios.xyz>
2026-01-01 03:52:19 -0600

    Added World transforms and visablilty

--- core/systems/scene_renderer/scene_renderer.c
+++ core/systems/scene_renderer/scene_renderer.c
@@ -6,6 +6,7 @@
 
 #include "core/systems/ecs/ecs.h"
 #include "core/systems/ecs/view.h"
+#include "core/systems/ecs/entity.h"
 #include "core/systems/ecs/components/c_tag.h"
 #include "core/systems/ecs/components/c_transform.h"
 #include "core/systems/ecs/components/c_mesh_renderer.h"
@@ -36,6 +37,52 @@ static mat4 sr_trs(const c_transform_t *tr)
     return mat4_mul(mat4_mul(T, R), S);
 }
 
+static int sr_is_visible_in_hierarchy(ecs_world_t *w, ecs_entity_t e)
+{
+    ecs_entity_t cur = e;
+
+    while (cur != 0 && ecs_entity_is_alive(w, cur))
+    {
+        c_tag_t *tag = ecs_get(w, cur, c_tag_t);
+        if (tag && tag->visible == 0)
+            return 0;
+
+        cur = ecs_entity_get_parent(w, cur);
+    }
+
+    return 1;
+}
+
+static mat4 sr_world_matrix(ecs_world_t *w, ecs_entity_t e)
+{
+    mat4 mats[128];
+    uint32_t mcount = 0;
+
+    ecs_entity_t cur = e;
+
+    while (cur != 0 && ecs_entity_is_alive(w, cur))
+    {
+        c_transform_t *tr = ecs_get(w, cur, c_transform_t);
+        if (tr)
+        {
+            if (mcount < (uint32_t)(sizeof(mats) / sizeof(mats[0])))
+                mats[mcount++] = sr_trs(tr);
+        }
+
+        cur = ecs_entity_get_parent(w, cur);
+    }
+
+    mat4 world = mat4_identity();
+
+    while (mcount)
+    {
+        mcount--;
+        world = mat4_mul(world, mats[mcount]);
+    }
+
+    return world;
+}
+
 void scene_renderer_render(renderer_t *r, ecs_world_t *scene)
 {
     if (!r || !scene)
@@ -53,7 +100,7 @@ void scene_renderer_render(renderer_t *r, ecs_world_t *scene)
         return;
 
     ecs_view_t v;
-    ecs_component_id_t ids[2] = {s_tr_id, s_mr_id};
+    ecs_component_id_t ids[2] = { s_tr_id, s_mr_id };
     if (!ecs_view_init(&v, scene, 2u, ids))
         return;
 
@@ -62,14 +109,12 @@ void scene_renderer_render(renderer_t *r, ecs_world_t *scene)
 
     while (ecs_view_next(&v, &e, c))
     {
-        c_transform_t *tr = (c_transform_t *)c[0];
         c_mesh_renderer_t *mr = (c_mesh_renderer_t *)c[1];
 
-        c_tag_t *tag = ecs_get(scene, e, c_tag_t);
-        if (tag && tag->visible == 0)
+        if (!sr_is_visible_in_hierarchy(scene, e))
             continue;
 
-        mat4 model_mtx = sr_trs(tr);
-        R_push_model(r, mr->model, model_mtx);
+        mat4 world_mtx = sr_world_matrix(scene, e);
+        R_push_model(r, mr->model, world_mtx);
     }
 }
